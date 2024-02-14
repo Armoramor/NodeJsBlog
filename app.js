355091c9ffe54324d1accb3527b2bb7a8f2a77b6
Marine Sance <marine.sance05@gmail.com>
2024-02-14 21:20:43 +0100

    changement app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(express.json());
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 // app.use(express.static('public'));
 
